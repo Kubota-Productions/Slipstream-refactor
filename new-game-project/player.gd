0e667dfa6961cea31cdcd123f91d7ac56ba33bb6
QBeLock <109404073+QBeLock@users.noreply.github.com>
2026-06-27 14:14:35 +0200

    atempt at grav rotation

--- new-game-project/player.gd
+++ new-game-project/player.gd
@@ -10,7 +10,7 @@ const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-var gravitational_diraction : Vector3
+var gravitational_diraction : Vector3 = Vector3.DOWN
 
 enum grav_states {
 	NORMAL,
@@ -18,6 +18,7 @@ enum grav_states {
 	FLOATING,
 	LANDING #for changing 
 }
+var current_grav_state : grav_states
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -27,11 +28,14 @@ func _input(event):
 		rotate_y(deg_to_rad(-event.relative.x * sesitivity))
 		pivot.rotate_x(deg_to_rad(-event.relative.y * sesitivity))
 		pivot.rotation.x = clamp(pivot.rotation.x, deg_to_rad(-90), deg_to_rad(45))
+	if Input.action_press("swich grav direction"):
+		gravitational_diraction = $"Camera origin/SpringArm3D/Camera3D".global_rotation #idk how to turn rotation to vector in simple way
+		
 
 func _physics_process(delta):
 	
-	if not is_on_floor():
-		velocity.y -= gravity * delta
+	if current_grav_state < 2 and not is_on_floor():
+		velocity -= gravitational_diraction * gravity * delta
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
